3b088dc82049b6a440495fc704968c9558f60ee7
artur <i@aderbin.ru>
2022-08-25 10:01:01 +0500

    eslint ignore

--- src/components/studentForm.jsx
+++ src/components/studentForm.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { MAX_AGE, MIN_AGE } from "../helpers/utils";
