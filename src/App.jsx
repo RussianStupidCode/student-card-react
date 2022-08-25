f73540285e5ef62f1b5b36874bc3ebf74d1827ff
artur <i@aderbin.ru>
2022-08-25 10:47:30 +0500

    change route

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { BASE_URL, ROUTER_PATH } from "./helpers/paths";
 import Card from "./layouts/card";
 import CreateForm from "./layouts/createForm";
-import Main from "./layouts/main";
 
 function App() {
   return (
     <Routes>
-      <Route exact path={BASE_URL} element={<Main />}>
-        <Route exact path="main" element={<Card />}></Route>
-        <Route exact path="form" element={<CreateForm />}></Route>
-        <Route
-          exact
-          path=""
-          element={<Navigate to={ROUTER_PATH.main} replace />}
-        ></Route>
-      </Route>
+      <Route exact path={ROUTER_PATH.main} element={<Card />} />
+      <Route
+        exact
+        path={BASE_URL}
+        element={<Navigate to={ROUTER_PATH.main} replace />}
+      />
+      <Route exact path={ROUTER_PATH.form} element={<CreateForm />} />
     </Routes>
   );
 }
